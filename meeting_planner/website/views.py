7240c7d4e8346d646f769c2f488fb488d9df369c
Schalk Pienaar <pienaarsw@gmail.com>
2020-11-19 22:36:29 +0000

    first commit

---
+++ meeting_planner/website/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+
+# Create your views here.
+
+def welcome(request):
+    return HttpResponse("Welcome to the Meeting Planner!")
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("My name is Schalk and I live in London")
